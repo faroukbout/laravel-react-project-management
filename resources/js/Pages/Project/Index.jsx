9af568c0edb432019c92c68d4014eb7301ed1d83
farouk <faroukboutoutaou@gmail.com>
2025-04-15 13:28:11 +0200

    added filters

--- resources/js/Pages/Project/Index.jsx
+++ resources/js/Pages/Project/Index.jsx
@@ -1,11 +1,27 @@
 import Pagination from '@/Components/Pagination'
+import SelectInput from '@/Components/SelectInput'
+import TextInput from '@/Components/TextInput'
 import { PROJECT_STATUS_CLASS_MAP, PROJECT_STATUS_TEXT_MAP } from '@/Constants'
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout'
-import { Head, Link } from '@inertiajs/react'
+import { Head, Link, router } from '@inertiajs/react'
 import React from 'react'
 
-export default function Index({auth,projects}) {
-    
+export default function Index({auth,projects,queryParams = null}) {
+    queryParams = queryParams || {};
+    const searchFieldChanged = (name,value) =>{
+        if(value){
+            queryParams[name] = value;
+        }else{
+            delete queryParams[name];
+        }
+
+        router.get(route("project.index"),queryParams);
+    };
+
+    const onKeyPress = (name,e) => {
+        if(e.key !== "Enter") return;
+        searchFieldChanged(name,e.target.value);
+    }
   return (
     <AuthenticatedLayout
     header={
@@ -21,6 +37,7 @@ export default function Index({auth,projects}) {
                     <div className="p-6 text-gray-900">
                         
                         <table className='w-full text-sm text-left rtl:text-right '>
+                        
                             <thead className='text-xs  uppercase bg-gray-100  border-b-2'>
                                 <tr className='text-nowrap'>
                                     <th className='px-3 py-3 text-gray-700'>Id</th>
@@ -31,6 +48,38 @@ export default function Index({auth,projects}) {
                                     <th className='px-3 py-3 text-gray-700'>Due Date</th>
                                     <th className='px-3 py-3 text-gray-700'>Created By</th>
                                     <th className='px-3 py-3 text-gray-700 text-center'>Actions</th>
+                                </tr>
+                            </thead>
+                            <thead className='text-xs  uppercase bg-gray-100  border-b-2'>
+                                <tr className='text-nowrap'>
+                                    <th className='px-3 py-3 text-gray-700'></th>
+                                    <th className='px-3 py-3 text-gray-700'></th>
+                                    <th className='px-3 py-3 text-gray-700'>
+                                        <TextInput 
+                                        className="w-full"
+                                        placeholder="Project Name"
+                                        defaultValue={queryParams.name}
+                                        onBlur={e => searchFieldChanged('name', e.target.value)
+                                        }
+                                        onKeyPress={e => onKeyPress('name',e)}
+                                        />
+                                    </th>
+                                    <th className='px-3 py-3 text-gray-700'>
+                                        <SelectInput className="w-full"
+                                        defaultValue={queryParams.status}
+                                        onChange={e => searchFieldChanged('status',e.target.value)}
+                                        >
+                                            <option value="">Status</option>
+                                            <option value="pending">Pending</option>
+                                            <option value="in_progress">In Progress</option>
+                                            <option value="completed">Completed</option>
+                                        </SelectInput>
+                                    </th>
+                                    <th className='px-3 py-3 text-gray-700'>
+                                    </th>
+                                    <th className='px-3 py-3 text-gray-700'></th>
+                                    <th className='px-3 py-3 text-gray-700'></th>
+                                    <th className='px-3 py-3 text-gray-700 text-center'></th>
                                 </tr>
                             </thead>
                             <tbody className='bg-white'>
@@ -47,7 +96,7 @@ export default function Index({auth,projects}) {
                                             }
                                         >{PROJECT_STATUS_TEXT_MAP[project.status]}</span>
                                         </td>
-                                        <td className='px-3 py-3 text-gray-800'>{project.created_at}</td>
+                                        <td className='px-3 py-3 text-gray-800 text-nowrap'>{project.created_at}</td>
                                         <td className='px-3 py-3 text-gray-800'>{project.due_date}</td>
                                         <td className='px-3 py-3 text-gray-800'>{project.createdBy.name}</td>
                                         <td className='px-3 py-3 text-gray-800'>
